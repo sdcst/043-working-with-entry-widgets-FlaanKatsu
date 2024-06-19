050b25a637ba7f83d15d07177ce5e5afe9050ddd
フランカツ!! <105261832+FlaanKatsu@users.noreply.github.com>
2024-06-19 12:51:54 -0700

    done?

--- task1.py
+++ task1.py
@@ -7,3 +7,57 @@ Create entry widets to allow user to enter their:
 
 Create a button so that when they click on the button, it states all of the information in a 4th entry widget
 """
+import tkinter as tk
+w = tk.Tk()
+w.attributes()
+w.geometry()
+w.resizable(False,False)
+w.title("student info entry")
+
+"""
+notes: as a bonus, i made a "error: not all fields have been entered!" if the user does not enter all fields,
+and made the display entry unmutable. (i like to make sure that UX is always as best i can make it)
+"""
+
+
+
+name = tk.Label(w,text="Name")
+StuN = tk.Label(w,text="Student Number")
+Gr = tk.Label(w,text="Grade")
+ne = tk.Entry(w)
+se = tk.Entry(w)
+ge = tk.Entry(w)
+do = tk.Button(w,text="Save Info")
+
+data = tk.Entry(width=80)
+a = "Data Summary will be showed here."
+data.insert(0,a)
+data.config(state='readonly')
+def ehe(useless):
+    dne = ne.get()
+    dse = se.get()
+    dge = ge.get()
+    if dne == "" or dse == "" or dge == "":
+        data.config(state='normal')
+        data.delete(0,tk.END)
+        data.config(state='disabled')
+        a = "error: not all fields have been entered!"
+    else:
+        data.config(state='normal')
+        data.delete(0,tk.END)
+        data.config(state='disabled')
+        a = f"your name is {dne}, your student number is \"{dse}\", and you are in grade {dge}."
+    data.config(state='normal')
+    data.insert(0,a)
+    data.config(state='readonly')
+do.bind("<Button-1>",ehe)
+
+data.grid(row=1,column=1,columnspan=3)
+name.grid(row=2,column=1)
+StuN.grid(row=2,column=2)
+Gr.grid(row=2,column=3)
+do.grid(row=4,column=2)
+ne.grid(row=3,column=1)
+se.grid(row=3,column=2)
+ge.grid(row=3,column=3)
+w.mainloop()
--- task2.py
+++ task2.py
@@ -6,3 +6,47 @@ The first 2 entry widgets allow the user to enter in the 2 short sides of a righ
 When the button is clicked, calculate the length of the hypotenuse and display it in the 3rd entry widget.
 Any labels you need for instruction are optional.
 """
+
+import tkinter as tk
+from tkinter import *
+w = tk.Tk()
+w.attributes()
+w.geometry()
+w.title("Basic Trig Calc")
+
+ins = tk.Label(w,text="INSTRUCTIONS")
+insd = tk.Label(w,text="enter the lengh of the 2 short sides of a right triangle.")
+s1 = tk.Entry(w)
+s2 = tk.Entry(w)
+c = tk.Button(w,text="Conpute!")
+ansbx = tk.Entry(w)
+
+ansbx.config(state="readonly")
+
+
+def conpute(s1,s2):
+    s32 = (s1**2)+(s2**2)
+    s3 = s32**0.5
+    s3 = round(s3,2)
+    return s3
+
+def do(e):
+    s1v = float(s1.get())
+    s2v = float(s2.get())
+    ans = conpute(s1v,s2v)
+    ansbx.config(state='normal')
+    ansbx.delete(0,END)
+    ansbx.insert(0,ans)
+    ansbx.config(state='readonly')
+
+
+c.bind("<Button-1>",do)
+
+ins.grid(row=1,column=2)
+insd.grid(row=2,column=1,columnspan=3)
+s1.grid(row=3,column=1)
+s2.grid(row=3,column=2)
+ansbx.grid(row=3,column=3)
+c.grid(row=4,column=2)
+
+w.mainloop()
